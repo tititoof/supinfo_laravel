5d73f9f6631664f2d821dd5841391ca1807d119f
Christophe Hartmann <chartmann.35@gmail.com>
2026-03-20 12:05:29 +0100

    wip

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
